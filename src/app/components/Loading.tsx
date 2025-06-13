f5ff8641aab5cfc8e0d48107a119a4695b606c25
Calingcooo <calingco.je@gmail.com>
2025-06-13 16:47:02 +0800

    added loading animation when when the page mounts

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,15 +1,39 @@
 import React from "react";
-import '../globals.css'
+import logo from "@/app/assets/3d_logo.png";
 
-const Loading = () => {
-
+const ImageDrawingLoader = () => {
   return (
-    <div className="bouncing-loader flex items-center justify-center bg-slate-800 h-screen">
-      <div className="w-6 h-6 m-1 bg-slate-200 rounded-full"></div>
-      <div className="w-6 h-6 m-1 bg-slate-200 rounded-full"></div>
-      <div className="w-6 h-6 m-1 bg-slate-200 rounded-full"></div>
+    <div className="flex items-center justify-center min-h-screen bg-[#0a192f]">
+      <svg
+        viewBox="0 0 78 90"
+        className="animate-reveal w-64 h-64"
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        <defs>
+          <clipPath id="polygonClip">
+            <polygon points="39 0, 0 22, 0 67, 39 90, 78 68, 78 23" />
+          </clipPath>
+        </defs>
+
+        <image
+          href={logo.src}
+          width="80"
+          height="110"
+          clipPath="url(#polygonClip)"
+          preserveAspectRatio="xMidYMid slice"
+          className="reveal-img"
+        />
+
+        <polygon
+          points="39 0, 0 22, 0 67, 39 90, 78 68, 78 23"
+          stroke="#64ffda"
+          strokeWidth="1"
+          fill="none"
+        />
+      </svg>
     </div>
   );
 };
 
-export default Loading;
+export default ImageDrawingLoader;
