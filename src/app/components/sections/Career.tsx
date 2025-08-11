ed7939224bda644651da17d6a7fa486c2b857eb0
Calingcooo <calingco.je@gmail.com>
2025-08-11 11:34:25 +0800

    fixed the career section overflows

--- src/app/components/sections/Career.tsx
+++ src/app/components/sections/Career.tsx
@@ -4,9 +4,9 @@ import CareerNav from "../CareerHighlights/CareerNav";
 
 const Career = () => {
   return (
-    <section className="w-full flex flex-col items-center justify-center min-h-svh py-20 lg:px-30">
-      <div className="min-w-full flex flex-col justify-center">
-        <SectionHeader sectionId ="career-highlights" title="Where I've Worked" />
+    <section className="w-full flex flex-col items-start justify-start md:items-center md:justify-center min-h-svh py-20 lg:px-30">
+      <div className="w-full flex flex-col justify-center">
+        <SectionHeader sectionId="career-highlights" title="Where I've Worked" />
         <CareerNav />
       </div>
     </section>
